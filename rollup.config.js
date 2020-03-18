883419e1df2a300139a82594939d5d3047a71abe
Dacio Romero <dacioromero@gmail.com>
2020-03-18 05:01:45 -0700

    feat: export umd and minified

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,43 @@
-// import typescript from '@rollup/plugin-typescript'
 import ts from '@wessberg/rollup-plugin-ts'
+import { terser } from 'rollup-plugin-terser'
 
 import packageJson from './package.json'
+
+function withMinified(options) {
+  const file = options.file.replace(/.js$/, '.min.js')
+  const plugins = [...(options.plugins || []), terser()]
+
+  return [
+    options,
+    {
+      ...options,
+      file,
+      plugins
+    }
+  ]
+}
 
 export default {
   input: 'src/index.ts',
   output: [
-    {
+    ...withMinified({
       file: packageJson.main,
       format: 'cjs',
       sourcemap: true
-    },
-    {
+    }),
+    ...withMinified({
       file: packageJson.module,
       format: 'es',
       sourcemap: true
-    }
+    }),
+    ...withMinified({
+      file: packageJson.unpkg,
+      format: 'umd',
+      name: 'ReactHookWM',
+      globals: {
+        react: 'React'
+      }
+    })
   ],
   plugins: [ts()],
   external: [
