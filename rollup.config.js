ca710381b105e6886b98aa58ff6e8fb7ad162eb0
Dacio Romero <dacioromero@gmail.com>
2020-03-18 18:06:20 -0700

    chore: change build folder structure

--- rollup.config.js
+++ rollup.config.js
@@ -1,47 +1,32 @@
 import ts from '@wessberg/rollup-plugin-ts'
 import { terser } from 'rollup-plugin-terser'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 
 import packageJson from './package.json'
-
-function withMinified(options) {
-  const file = options.file.replace(/.js$/, '.min.js')
-  const plugins = [...(options.plugins || []), terser()]
-
-  return [
-    options,
-    {
-      ...options,
-      file,
-      plugins
-    }
-  ]
-}
 
 export default {
   input: 'src/index.ts',
   output: [
-    ...withMinified({
+    {
       file: packageJson.main,
       format: 'cjs',
       sourcemap: true
-    }),
-    ...withMinified({
+    },
+    {
       file: packageJson.module,
       format: 'es',
       sourcemap: true
-    }),
-    ...withMinified({
+    },
+    {
       file: packageJson.unpkg,
       format: 'umd',
       name: 'ReactHookWM',
+      plugins: [terser()],
+      sourcemap: true,
       globals: {
         react: 'React'
       }
-    })
+    }
   ],
-  plugins: [ts()],
-  external: [
-    ...Object.keys(packageJson.dependencies || {}),
-    ...Object.keys(packageJson.peerDependencies || {})
-  ]
+  plugins: [ts(), peerDepsExternal({ includeDependencies: true })]
 }
